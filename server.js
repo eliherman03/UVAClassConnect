262dc75753ca1d1bfaa43eaf448fd696fcb9fb26
eliherman <hzt7wt@virginia.edu>
2023-08-31 19:13:49 -0400

    transfered files over to github repo

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect(process.env.MONGO_ACCESS, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
